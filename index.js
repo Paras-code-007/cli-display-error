34d55408176537193ce0f17b363ff83a641e3d51
Paras-code-007 <paras.arora0025@gmail.com>
2021-02-10 02:57:09 +0530

    used ansi escape sequence instead of whole chalk lib

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const {redBright,yellowBright,red, bgRedBright}= require('chalk')
 const {error,warning}= require('log-symbols')
-
+const red= '\x1b[31m'
+const yellow= '\x1b[33m'
+const bgRed= '\x1b[41m'
+const bold= '\x1b[1m'
+const end= '\x1b[0m'
+const reverse= '\x1b[7m'
 /*
 * @param {String} heading
 * @param {Error} err
@@ -11,12 +15,14 @@ const {error,warning}= require('log-symbols')
 module.exports= (heading='ERROR: ',err,display=true,exitStatus=true)=>{
     if(err){
         if(display){
-            console.log(`\n${error} ${bgRedBright.bold(heading)}\n`)
-            console.log(`${redBright('Error Name:')} ${err.name}`)
-            console.log(`${redBright('Message:')} ${err.message}`)
-            console.log(`${redBright('Error Stack ⬇')}\n${err.stack}\n`)
+            console.log(`\n${error} ${red}${reverse}${bold}%s${end} \n`,` ${heading} `)
+            console.log(`${red}${bold}%s${end}`,`Error Name:`, `${err.name}`)
+            console.log(`${red}${bold}%s${end}`,`Message:`,`${err.message}`)
+            console.log(`${red}${bold}%s${end}`,`Error Stack ⬇`)
+            console.log(err.stack)
+            // console.log(`${redBright()
         }else{
-            console.log(`\n${warning} ${yellowBright.inverse.bold(heading)}\n`)
+            console.log(`\n${warning} ${reverse}${yellow}${bold}%s${end} \n`,` ${heading} `)
         }
 
         exitStatus && process.exit(0)  //handled the error therfore exitstatus should not be 1 but 0
@@ -25,6 +31,3 @@ module.exports= (heading='ERROR: ',err,display=true,exitStatus=true)=>{
         }
     }
 }
-
-
-
